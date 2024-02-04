a5857b413055998c145ec31d193a0a94f983b692
Oracso <85285983+Oracso@users.noreply.github.com>
2024-02-04 19:12:40 +0000

    Initial Commit

---
+++ WatchHaptics Watch App/WatchHapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchHapticsApp.swift
+//  WatchHaptics Watch App
+//
+//  Created by Oscar Hardy on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WatchHaptics_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
